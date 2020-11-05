788efe69e197b30000102cce33575dd5742e8513
morgan9900103 <menghan.lee@ufl.edu>
2020-11-05 18:09:39 -0500

    update common_include.h

--- include/common_include.h
+++ include/common_include.h
@@ -6,11 +6,6 @@
 using Eigen::Vector2d;
 using Eigen::Vector3d;
 
-#include <sophus/se3.h>
-#include <sophus/so3.h>
-using Sophus::SE3;
-using Sophus::SO3;
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/types.hpp>
 using cv::Mat;
